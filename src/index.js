3706814d1dd1ed6e6a8cb00b295a93c4dd780563
Krishna Shekhram <5135954+kshekhram@users.noreply.github.com>
2019-08-03 03:24:58 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ document.getElementById("app").innerHTML = `
   This is CS 498 Summer 19 Data Visualization Project(7-3-3)
 </div>
 `;
+
+var data = [4,8,5,10,6];
+var svg = d3.select("svg");
+svg.selectAll("rect").data(data).enter().append()
+   .attr("width", 19)
+   .attr("height", function(d){return 10*d;})
+   .attr("x", function(d, i){return 20*i;})
+   .attr("y", function(d){return (420 - 10*d);});
